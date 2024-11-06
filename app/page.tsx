d5c416837e71176338e84cdc4bb120db95dae728
alinafe98alhaji <emmanueldicksonmapulanga@gmail.com>
2024-11-06 04:07:35 +0200

    empty fix

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/dataownershipandmanagement" // Link to your survey page
+            href="/datacollectionques" // Link to your survey page
             target="_self" // Change to _self if you want to navigate in the same tab
           >
             Start Survey
